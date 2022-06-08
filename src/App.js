8ebb2b92727c351281a4cfaa6683a88802ef31fa
Kacper Zapart <k4p1email@gmail.com>
2022-06-08 18:56:18 +0200

    DAI supply option added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import {useState} from "react";
-import { Route } from "react-router";
 import './App.css';
 import WalletConnect from './components/walletconnect.js'; //navbar
 import React from 'react';
-import Lending from './components/Lending.js';
+import ButtonsSupplyAndWithdraw from "./components/Supply.js";
+
+
+
+
 
 
 function App() {
   const [accounts, setAccounts] = useState([]);
-  const content = "main";
+  const [balanceOfDai, setBalanceOfDai] = useState(0);
+  const [balanceOfStakedDai, setBalanceOfStakedDai] = useState(0);
+  const isAllowed = Boolean(false);
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
   return (
     <div className="App">
-      <WalletConnect accounts={accounts} setAccounts={setAccounts}/>
+      <WalletConnect isAllowed={isAllowed} accounts={accounts} setAccounts={setAccounts} balanceOfDai={balanceOfDai} setBalanceOfDai={setBalanceOfDai} balanceOfStakedDai={balanceOfStakedDai} setBalanceOfStakedDai={setBalanceOfStakedDai} />
       <div className="Content">
-        
-        <div className="DAI">
-          <div>Dai</div>
-          <div>Supply Balance : </div>
-          <div><button>Supply</button></div>
+      <div className="DAI">
+            <div className="daicontent">Dai</div>
+            <div className="daicontent">Balance : {balanceOfDai} DAI </div>
+            <div className="daicontent">Supplied Balance : {balanceOfStakedDai} DAI </div>
+            <div className="daicontent">
+
+            </div>
+            <div className="">
+            <ButtonsSupplyAndWithdraw isAllowed={isAllowed} accounts={accounts} setAccounts={setAccounts} inputs={inputs}  />
+            </div>
         </div>
+        
+          
+        
       </div>
     </div>
   );
--- src/components/Supply.js
+++ src/components/Supply.js
@@ -1,10 +1,115 @@
 import React from "react";
+import { ethers, BigNumber } from "ethers";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
 
 
-function SupplyButton() {
-    return(
+
+const vaultAbi = '[ { "inputs": [ { "internalType": "uint256", "name": "_rates", "type": "uint256" } ], "name": "changeRates", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" } ], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "amountOfDai", "type": "uint256" } ], "name": "supplyDai", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "newOwner", "type": "address" } ], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "withdrawAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "amountOfDaiToWithdraw", "type": "uint256" } ], "name": "withdrawSuppliedDai", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "BalanceOfStakedDai", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "dai", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "rates", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "usdc", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" } ]';
+const DaiAbi = '[{"inputs":[{"internalType":"uint256","name":"chainId_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"guy","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":true,"inputs":[{"indexed":true,"internalType":"bytes4","name":"sig","type":"bytes4"},{"indexed":true,"internalType":"address","name":"usr","type":"address"},{"indexed":true,"internalType":"bytes32","name":"arg1","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"arg2","type":"bytes32"},{"indexed":false,"internalType":"bytes","name":"data","type":"bytes"}],"name":"LogNote","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"dst","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"deny","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"move","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"holder","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"bool","name":"allowed","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"pull","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"push","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"rely","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"wards","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
+
+
+
+
+
+
+
+const ButtonsSupplyAndWithdraw = ({ accounts, setAccounts, balanceOfDai, inputs, isAllowed}) =>  {
+    const DaiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const VaultAddress = "0x8e531f32A9a950f6E1bd40F1dfC18F40272F430D";
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    console.log(provider);
+    const signer = provider.getSigner();
+    const daiContract = new ethers.Contract(DaiAddress, DaiAbi, signer) ;
+    console.log(signer);
+    
+
+        async function supplyButton() {
+            const signer = provider.getSigner();
+            const vaultContract = new ethers.Contract(VaultAddress, vaultAbi, signer) ;
+            console.log(inputs.value)
+            const amount = BigNumber.from(values.amount).mul(BigNumber.from(10).pow(18));
+            await daiContract.approve(VaultAddress,BigNumber.from(amount));
+            const tx = vaultContract.supplyDai(BigNumber.from(amount), {
+                gasLimit: 1000000,
+            });    
+        }
+        function withdrawButton() {
+            const signer = provider.getSigner();
+            const vaultContract = new ethers.Contract(VaultAddress, vaultAbi, signer) ;
+            console.log(values.amount)
+            const amount = BigNumber.from(values.amount).mul(BigNumber.from(10).pow(18));
+            console.log(amount)
+            vaultContract.withdrawSuppliedDai(BigNumber.from(amount), {
+                gasLimit: 1000000,
+            });
+        }
         
+        function approveButton() {
+            const signer = provider.getSigner();
+            daiContract.approve(VaultAddress,BigNumber.from(99999999999))
+        }
+        const style = {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          };
+          
+        const [open, setOpen] = React.useState(false);
+        const [switches, setSwitches] = React.useState("");
+        const handleOpen = () => setOpen(true);
+        const handleClose = () => setOpen(false);
+        const [values, setValues] = React.useState({
+            amount: '',
+            password: '',
+            weight: '',
+            weightRange: '',
+            showPassword: false,
+          });
+        const handleSwitch = (string) => {
+            setSwitches(string);
+        }
+        const handleChange = (prop) => (event) => {
+            setValues({ ...values, [prop]: event.target.value });
+          };
+    return(
+        <div className="daicontent">
+                <div>
+      <Button variant="contained" onClick={handleOpen}>Supply</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div>
+              <Button onClick={supplyButton} >Supply</Button>
+              <Button onClick={withdrawButton} >Withdraw</Button>
+          </div>
+          
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={values.amount}
+            onChange={handleChange('amount')}
+            startAdornment={<InputAdornment position="start">DAI</InputAdornment>}
+            label="Amount"
+          />
+        </Box>
+      </Modal>
+    </div>
+        </div>
     )
-}
+};
 
-export default SupplyButton;
+export default ButtonsSupplyAndWithdraw;
